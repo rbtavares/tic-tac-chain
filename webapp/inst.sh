a0fd1857252f32ea8cf56cd53284d4b1e949e14d
rbtavares <rbtavares@users.noreply.github.com>
2024-01-12 12:44:38 +0000

    i dont know why this was here

--- webapp/inst.sh
+++
@@ -1,5 +0,0 @@
-npm i @fortawesome/fontawesome-svg-core
-npm i @fortawesome/free-solid-svg-icons
-npm i @fortawesome/free-regular-svg-icons
-npm i @fortawesome/free-brands-svg-icons
-npm i @fortawesome/react-fontawesome@latest
